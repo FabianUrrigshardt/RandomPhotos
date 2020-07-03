bd37b9f5de8d641b90d818947bff5cd26066a2ea
unknown <fabian.urrigshardt@stud.uni-due.de>
2020-07-04 00:33:09 +0200

    Probleme mit dem Pfad

--- main.py
+++ main.py
@@ -1,33 +1,65 @@
 from tkinter import Tk
 from tkinter import Image 
 from tkinter import Canvas 
+from tkinter import Button
 from PIL import ImageTk,Image 
 import os
-
-
+import collections
+import random
 
 def smallerImg(img):
-    x= int(img.size[0]/5)
-    y= int(img.size[1]/5)
+    x=img.size[0]
+    y=img.size[1]
+    z=1 #Faktor der Vergrößerung
+    
+    if x>3400:
+        z=4
+    elif x>2000:
+        z=3
+    elif x>1200:
+        z=2
+    
+    x=int(img.size[0]/z)
+    y=int(img.size[1]/z)
 
-    ret = img.resize((x,y))
-#Todo: Je nach Größe Parameter anpassen
+    return img.resize((x,y))
+
+def testComand():
+    return
+
+def getImgList():
+    ret = collections.deque()
+    for sfld in os.listdir('images'):
+        for im in os.listdir(os.path.join('images',sfld)):
+            print(im)
+            ret.append(im)         
+    return ret    
+
+def getRandomImg(imgList):
+    #rand = random.randint(1,imgList.maxlen())
+    rand = 1
+    while rand > 0:
+        ret=imgList.pop()
+        rand = rand -1
     return ret
 
+imgList = getImgList()
 
 window = Tk()
 window.title("Random Photo App")
 window.geometry('800x800')
 
-
-im = Image.open("a.jpg")
-
+im = Image.open(getRandomImg(imgList))
 out = smallerImg(im)
 img = ImageTk.PhotoImage(out)
-#img = img.resize(100,100)
+
 
 canvas = Canvas(window, width=600, height=600)
 canvas.pack()
 canvas.create_image(300,300, image=img)
+
+btNext = Button(window, text="nächstes Bild", command=testComand())
+btNext.pack()
+
 
 window.mainloop()
